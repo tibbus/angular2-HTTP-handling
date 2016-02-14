34e4c397be8944e6ce68da1514febd7eeb3e1f2c
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2016-02-14 13:11:25 +0000

    add jsonP example, wikipedia search

--- src/dev/app/app.component.ts
+++ src/dev/app/app.component.ts
@@ -1,13 +1,15 @@
 ﻿import {Component} from 'angular2/core';
 import {TohComponent} from './toh/toh.component';
+import {WikiComponent} from './wiki/wiki.component';
 
 @Component({
     selector: 'my-app',
     template: `<div>
-                <my-toh></my-toh>    
+                <my-toh></my-toh> 
+                <my-wiki></my-wiki>   
             </div>
               `,
-    directives: [TohComponent]
+    directives: [TohComponent, WikiComponent]
 })
 
 export class AppComponent {
